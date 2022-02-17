07f25ab464009c0ebd31fad5321f2d041d9b7d3a
Joao Marcos Africo da Silva <joao.africo@keeggo.com>
2022-02-17 16:52:04 -0300

    adicionando configuracoes do android

--- src/main/java/com/alura/appium/CursoAppiumAlura/App.java
+++ src/main/java/com/alura/appium/CursoAppiumAlura/App.java
@@ -1,9 +1,30 @@
 package com.alura.appium.CursoAppiumAlura;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+
 public class App 
 {
-    public static void main( String[] args )
+
+	public static void main( String[] args ) throws MalformedURLException
     {
-        System.out.println( "Hello World!" );
+		File apk = new File("C:\\Aprendizado\\Cursos Java\\CursoAppiumAlura\\resources\\alura_esporte.apk");		
+        
+		DesiredCapabilities configuracoes = new DesiredCapabilities();
+        configuracoes.setCapability(MobileCapabilityType.APP, apk.getAbsolutePath());
+        configuracoes.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+        configuracoes.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        
+		URL URLConexao = new URL("http://127.0.0.1:4723/wd/hub");
+
+		AppiumDriver<?> driver = new AppiumDriver<>(URLConexao,configuracoes);
+		
     }
 }
